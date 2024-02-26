b4dcee8b7c25368241d464593173ebca50ff2168
pexeixv <gavineditz@gmail.com>
2024-02-26 22:42:11 +0530

    install Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://amazingcreditcards.in",
+  integrations: [tailwind(), sitemap()],
 });

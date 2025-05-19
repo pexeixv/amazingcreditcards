320e011d192b77e69a0025bdb07acf81c89c4f64
pexeixv <gavineditz@gmail.com>
2025-05-19 23:44:29 +0530

    Image optimized for each size

--- src/js/hygraph.js
+++ src/js/hygraph.js
@@ -1,53 +1,75 @@
 import { request, gql } from "graphql-request";
+const url = import.meta.env.CONTENT_API;
 
-const endpoint = import.meta.env.CONTENT_API;
-
-const transform = `
-  transformation: {
-    image: {
-      resize: { width: 800 }, 
-      quality: { value: 70 }
-    }, 
-    document: { 
-      output: { format: webp } 
-    }
-  }
-`;
+const transformations = {
+  emoji: `(transformation: {
+    image: { resize: { height: 40 }, quality: { value: 20 } },
+    document: { output: { format: webp } }
+  })`,
+  logo: `(transformation: {
+    image: { resize: { height: 50 }, quality: { value: 50 } },
+    document: { output: { format: webp } }
+  })`,
+  shortLogo: `(transformation: {
+    image: { resize: { height: 20 }, quality: { value: 20 } },
+    document: { output: { format: webp } }
+  })`,
+  image: `(transformation: {
+    image: { resize: { height: 120 }, quality: { value: 40 } },
+    document: { output: { format: webp } }
+  })`,
+  ogImage: `(transformation: {
+    image: { resize: { width: 200 }, quality: { value: 40 } },
+    document: { output: { format: webp } }
+  })`,
+  bannerImage: `(transformation: {
+    image: { resize: { width: 600 }, quality: { value: 60 } },
+    document: { output: { format: webp } }
+  })`,
+  blogImage: `(transformation: {
+    image: { resize: { width: 600 }, quality: { value: 60 } },
+    document: { output: { format: webp } }
+  })`,
+  hyperlinkedImage: `(transformation: {
+    image: { resize: { width: 600 }, quality: { value: 60 } },
+    document: { output: { format: webp } }
+  })`,
+};
 
 const queries = {
   categories: gql`
     query Categories {
-      categories(where: { draft: false }, first: 9999) {
+      categories(where: { draft: false }, first: 9999, stage: PUBLISHED) {
         name
         pageTitle
         slug
         emoji {
-          url (${transform})
+          url ${transformations.emoji}
         }
         image {
-          url (${transform})
+          url ${transformations.image}
         }
         ogImage {
-          url (${transform})
+          url ${transformations.ogImage}
         }
       }
     }
   `,
   banks: gql`
     query Banks {
-      banks(where: { draft: false }, first: 9999) {
+      banks(where: { draft: false }, first: 9999, stage: PUBLISHED) {
         name
         slug
         pageTitle
         description
         logo {
-          url (${transform})
-        }
-        ogImage {
-          url (${transform})
+          url ${transformations.logo}
         }
         shortLogo {
-          url (${transform})
+          url ${transformations.shortLogo}
+        }
+        ogImage {
+          url ${transformations.ogImage}
         }
         summary {
           html
@@ -57,7 +79,7 @@ const queries = {
   `,
   featuredCards: gql`
     query FeaturedCards {
-      cards(where: { featured: true, draft: false }, first: 12) {
+      cards(where: { featured: true, draft: false }, first: 12, stage: PUBLISHED) {
         name
         slug
         applyUrl
@@ -66,207 +88,134 @@ const queries = {
           html
         }
         image {
-          url (${transform})
+          url ${transformations.image}
         }
       }
     }
   `,
   featuredCategories: gql`
     query FeaturedCategories {
-      categories(first: 6) {
+      categories(first: 6, stage: PUBLISHED) {
         name
         slug
         emoji {
-          url (${transform})
+          url ${transformations.emoji}
         }
       }
     }
   `,
   cards1: gql`
     query Cards {
-      cards(where: { draft: false }, first: 100, skip: 0) {
+      cards(where: { draft: false }, first: 100, skip: 0, stage: PUBLISHED) {
         name
         slug
         applyUrl
         rating
-        description {
-          html
-        }
-        summary {
-          html
-        }
-        content {
-          html
-        }
-        image {
-          url (${transform})
-        }
-        ogImage {
-          url (${transform})
-        }
+        description { html }
+        summary { html }
+        content { html }
+        image { url ${transformations.image} }
+        ogImage { url ${transformations.ogImage} }
       }
     }
   `,
   cards2: gql`
     query Cards {
-      cards(where: { draft: false }, first: 100, skip: 100) {
+      cards(where: { draft: false }, first: 100, skip: 100, stage: PUBLISHED) {
         name
         slug
         applyUrl
         rating
-        description {
-          html
-        }
-        summary {
-          html
-        }
-        content {
-          html
-        }
-        image {
-          url (${transform})
-        }
-        ogImage {
-          url (${transform})
-        }
+        description { html }
+        summary { html }
+        content { html }
+        image { url ${transformations.image} }
+        ogImage { url ${transformations.ogImage} }
       }
     }
   `,
   cards3: gql`
     query Cards {
-      cards(where: { draft: false }, first: 100, skip: 200) {
+      cards(where: { draft: false }, first: 100, skip: 200, stage: PUBLISHED) {
         name
         slug
         applyUrl
         rating
-        description {
-          html
-        }
-        summary {
-          html
-        }
-        content {
-          html
-        }
-        image {
-          url (${transform})
-        }
-        ogImage {
-          url (${transform})
-        }
+        description { html }
+        summary { html }
+        content { html }
+        image { url ${transformations.image} }
+        ogImage { url ${transformations.ogImage} }
       }
     }
   `,
   cards4: gql`
     query Cards {
-      cards(where: { draft: false }, first: 100, skip: 300) {
+      cards(where: { draft: false }, first: 100, skip: 300, stage: PUBLISHED) {
         name
         slug
         applyUrl
         rating
-        description {
-          html
-        }
-        summary {
-          html
-        }
-        content {
-          html
-        }
-        image {
-          url (${transform})
-        }
-        ogImage {
-          url (${transform})
-        }
+        description { html }
+        summary { html }
+        content { html }
+        image { url ${transformations.image} }
+        ogImage { url ${transformations.ogImage} }
       }
     }
   `,
   cards: gql`
     query Cards {
-      cards(where: { draft: false }, first: 100) {
+      cards(where: { draft: false }, first: 100, stage: PUBLISHED) {
         name
         slug
         applyUrl
         rating
-        description {
-          html
-        }
-        summary {
-          html
-        }
-        content {
-          html
-        }
-        image {
-          url (${transform})
-        }
-        ogImage {
-          url (${transform})
-        }
+        description { html }
+        summary { html }
+        content { html }
+        image { url ${transformations.image} }
+        ogImage { url ${transformations.ogImage} }
       }
     }
   `,
   banksWithCards: gql`
     query BanksWithCards {
-      banks(where: { draft: false }, first: 9999) {
+      banks(where: { draft: false }, first: 9999, stage: PUBLISHED) {
         name
         slug
         pageTitle
-        ogImage {
-          url (${transform})
-        }
-        logo {
-          url (${transform})
-        }
+        logo { url ${transformations.logo} }
+        ogImage { url ${transformations.ogImage} }
         cards(where: { draft: false }, first: 9999) {
-          ... on Card {
-            name
-            slug
-            description {
-              html
-            }
-            image {
-              url (${transform})
-            }
-            applyUrl
-            rating
-          }
+          name
+          slug
+          description { html }
+          image { url ${transformations.image} }
+          applyUrl
+          rating
         }
       }
     }
   `,
   categoriesWithCards: gql`
     query CategoriesWithCards {
-      categories(where: { draft: false }, first: 9999) {
+      categories(where: { draft: false }, first: 9999, stage: PUBLISHED) {
         name
         slug
         pageTitle
-        image {
-          url (${transform})
-        }
-        bannerImage {
-          url (${transform})
-        }
-        content1 {
-          html
-        }
-        content2 {
-          html
-        }
-
+        image { url ${transformations.image} }
+        bannerImage { url ${transformations.bannerImage} }
+        content1 { html }
+        content2 { html }
         card_category(first: 9999) {
           ... on Card {
             name
             rating
-            description {
-              html
-            }
+            description { html }
             slug
             applyUrl
-            image {
-              url (${transform})
-            }
+            image { url ${transformations.image} }
           }
         }
       }
@@ -274,28 +223,20 @@ const queries = {
   `,
   blogs: gql`
     query Blogs {
-      blogs(where: { draft: false }, first: 9999, orderBy: createdAt_DESC) {
+      blogs(where: { draft: false }, first: 9999, orderBy: createdAt_DESC, stage: PUBLISHED) {
         title
         slug
         excerpt
-        image {
-          url (${transform})
-        }
+        image { url ${transformations.blogImage} }
         draft
-        ogImage {
-          url (${transform})
-        }
+        ogImage { url ${transformations.ogImage} }
         content(first: 999) {
           ... on BlogContent {
-            textContent {
-              html
-            }
+            textContent { html }
           }
           ... on HyperlinkedImage {
             url
-            image {
-              url (${transform})
-            }
+            image { url ${transformations.hyperlinkedImage} }
             description
           }
         }
@@ -306,7 +247,7 @@ const queries = {
 
 const hygraph = async (query, queryParam) => {
   try {
-    const res = await request(endpoint, queries[query]);
+    const res = await request(url, queries[query]);
     return res[queryParam || query] || [];
   } catch (error) {
     console.error("Error fetching data from Hygraph:", error);
